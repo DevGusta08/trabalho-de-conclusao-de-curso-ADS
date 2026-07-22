b8e17558defa44e5c9712be72d1ad721f27c413b
Gustavo Gabriel Miranda <gu.miranda0706@gmail.com>
2026-07-22 18:02:19 -0300

    feat: adiciona a rota de busca de usuários por ID

--- controllers/usuarios.controller.js
+++ controllers/usuarios.controller.js
@@ -10,4 +10,19 @@ async function listar(req, res) {
     }
 };
 
-module.exports = {listar};
+async function buscarPorId(req, res) {
+    try {
+        const {id} = req.params;
+        const [usuarios] = await pool.query('SELECT * FROM usuarios WHERE id = ?', [id]);
+
+        if (usuarios.length === 0) {
+            return res.status(404).json({ error: 'Usuário não encontrado' });
+        }
+        res.json(usuarios);
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({error:'Deu erro'});
+    }
+};
+
+module.exports = {listar, buscarPorId};
--- routes/usuarios.routes.js
+++ routes/usuarios.routes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const usuariosController = require('../controllers/usuarios.controller');
 
 router.get('/usuarios', usuariosController.listar);
+router.get('/usuarios/:id', usuariosController.buscarPorId);
 
 module.exports = router;
