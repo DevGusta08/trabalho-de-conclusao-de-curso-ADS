c2eb4c9f843151c021f2548c19e81f6773d7a524
Gustavo Gabriel Miranda <gu.miranda0706@gmail.com>
2026-08-26 23:02:57 -0300

    Adiciona rota POST /login com autenticação via bcrypt

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ const pool = require('./config/db')
 const app = express();
 const port = process.env.PORT || 3000;
 const usuariosRoutes = require('./routes/usuarios.routes');
+const loginRoutes = require('./routes/login.routes');
 
 // Método básico de Rota com GET
 app.get('/', (req, res) => {
     res.send('Servidor ligado!');
 });
 app.use(express.json())
-
+app.use(loginRoutes);
 app.use(usuariosRoutes);
 app.listen(port, () => { console.log(`Servidor rodando na porta ${port}`) });
