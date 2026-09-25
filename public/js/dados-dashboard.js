b79834551acd1ceb65e87ae945c03a5765c66228
Gustavo Gabriel Miranda <gu.miranda0706@gmail.com>
2026-09-25 18:12:02 -0300

    adciona mais matérias como dados

--- public/js/dados-dashboard.js
+++ public/js/dados-dashboard.js
@@ -24,4 +24,64 @@ const areas = [
       },
     ],
   },
-]
+  {
+    nome: "Humanas",
+    materias: [
+      {
+        nome: "História",
+        recomendacoes: [
+          {
+            tipo: "video",
+            titulo: "Era Vargas - Resumo para o vestibular",
+            link: "https://www.youtube.com/watch?v=EXEMPLO2",
+          },
+          {
+            tipo: "artigo",
+            titulo: "Linha do tempo da Independência do Brasil",
+            link: "https://exemplo.com/artigo-historia",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    nome: "Biológicas",
+    materias: [
+      {
+        nome: "Biologia",
+        recomendacoes: [
+          {
+            tipo: "video",
+            titulo: "Genética Mendeliana - Aula 1",
+            link: "https://www.youtube.com/watch?v=EXEMPLO3",
+          },
+          {
+            tipo: "artigo",
+            titulo: "Ecologia: cadeias e teias alimentares",
+            link: "https://exemplo.com/artigo-ecologia",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    nome: "Linguagens",
+    materias: [
+      {
+        nome: "Português",
+        recomendacoes: [
+          {
+            tipo: "video",
+            titulo: "Como estruturar uma redação nota 1000",
+            link: "https://www.youtube.com/watch?v=EXEMPLO4",
+          },
+          {
+            tipo: "artigo",
+            titulo: "Figuras de linguagem mais cobradas no Enem",
+            link: "https://exemplo.com/artigo-figuras",
+          },
+        ],
+      },
+    ],
+  },
+];
