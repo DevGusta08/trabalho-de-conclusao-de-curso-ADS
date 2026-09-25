87203891788b273bcb1ad65eb0b987387b0c6abf
Gustavo Gabriel Miranda <gu.miranda0706@gmail.com>
2026-09-25 18:00:54 -0300

    cria o dado: Exatas e matemática e o arquivo dashboard.css

---
+++ public/js/dados-dashboard.js
@@ -0,0 +1,27 @@
+const areas = [
+  {
+    nome: "Exatas",
+    materias: [
+      {
+        nome: "Matemática",
+        recomendacoes: [
+          {
+            tipo: "video",
+            titulo: "Funções do 1º grau - Aula completa",
+            link: "https://www.youtube.com/watch?v=EXEMPLO1",
+          },
+          {
+            tipo: "artigo",
+            titulo: "Resumo de Geometria Plana para o Enem",
+            link: "https://exemplo.com/artigo-geometria",
+          },
+          {
+            tipo: "livro",
+            titulo: "Matemática - Fundamentos e Aplicações (Vol. 1)",
+            link: "https://exemplo.com/livro-matematica",
+          },
+        ],
+      },
+    ],
+  },
+]
